bfd22f51ae48c7e8e50d68cf76206507dd4934b3
Vraj Vyas <vyasvraj92@gmail.com>
2024-07-30 15:25:59 +0530

    added custom form field shadcn form,input,label,select and ZOD

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,30 @@
+import Header from '@/components/shared/Header'
 import React from 'react'
+import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm'
+import { auth } from '@clerk/nextjs/server'
+import { getUserById } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
+const AddTransformationPage = async({params:{type}}:SearchParamProps) => {
+ const transformation=transformationTypes[type]
+ const {userId}=auth()
 
-const AddTransformationPage = () => {
+ if (!userId) redirect("/sign-in")
+ const user=await getUserById(userId)
   return (
-    <div>AddTransformationPage</div>
+    <>
+      <Header
+        title={transformation.title}
+        subtitle={transformation.subTitle} 
+        />
+        <TransformationForm 
+        action='Add' 
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+        />
+    
+    </>
   )
 }
 
